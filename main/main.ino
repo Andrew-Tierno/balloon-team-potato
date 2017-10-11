00723cf643bdb94ffeb1510f195ba72be9bf4772
Andrew Tierno <andrew.tierno@gmail.com>
2017-10-11 12:10:51 -0700

    Added BMP and collection buffer to reduce noise

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,76 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
 
+//Data will be outputted every COLLECTION_POINTS * LOOP_DELAY milliseconds
+#define COLLECTION_POINTS 10
+#define LOOP_DELAY 100
+
+//Sensors
+Adafruit_BMP280 bmp;
+
+//Pins
+int led = 13;
+
+//Data Buffers
+float pressureBuffer[COLLECTION_POINTS];
+float temperatureBuffer[COLLECTION_POINTS];
+float altitudeBuffer[COLLECTION_POINTS];
+
+//Housekeeping Variables
+int loopNum;
+bool buffersReady;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+
+  loopNum = 0;
+  buffersReady = false;
+  
+  if (!bmp.begin()) {  
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (true);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (loopNum == 0 && buffersReady) {
+    digitalWrite(led, HIGH);
+    Serial.print(F("Temperature = "));
+    Serial.print(getAverage(temperatureBuffer));
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(getAverage(pressureBuffer));
+    Serial.println(" kPa");
+
+    Serial.print(F("Altitude = "));
+    Serial.print(getAverage(altitudeBuffer)); 
+    Serial.println(" m");
+  }
+  else {
+    digitalWrite(led, LOW);
+    temperatureBuffer[loopNum] = bmp.readTemperature(); 
+    pressureBuffer[loopNum] = bmp.readPressure() / 1000;//Readings are in Pa
+    altitudeBuffer[loopNum] = bmp.readAltitude(1013.25);// TODO: this should be adjusted
+  }
+  
+  loopNum = (loopNum + 1) % COLLECTION_POINTS;
+  if (loopNum == 0) {
+    buffersReady = true;
+  }
+
+  delay(LOOP_DELAY);
 }
+
+float getAverage(float[] list) {
+  float sum = 0;
+  int len = sizeof(list) / sizeof(float);
+  for (int i = 0; i < len; i++) {
+    sum += list[i];
+  }
+  return sum / len;
+}
+
