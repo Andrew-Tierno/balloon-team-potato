3d15bb6ba6c88af25bb2657fa5fcf313d3f78274
bnewm0609 <nlb9991@gmail.com>
2017-10-13 21:04:45 -0700

    Added lib folder with necessary libraries

--- main/main.ino
+++ main/main.ino
@@ -7,12 +7,14 @@
 #define COLLECTION_POINTS 10
 #define LOOP_DELAY 100
 
+// Prototypes
+float getAverage(float list[]);
+
 //Sensors
 Adafruit_BMP280 bmp;
 
 //Pins
 int led = 13;
-
 //Data Buffers
 float pressureBuffer[COLLECTION_POINTS];
 float temperatureBuffer[COLLECTION_POINTS];
@@ -25,7 +27,7 @@ bool buffersReady;
 void setup() {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-
+  pinMode(gps, INPUT);
   loopNum = 0;
   buffersReady = false;
   
@@ -56,16 +58,18 @@ void loop() {
     pressureBuffer[loopNum] = bmp.readPressure() / 1000;//Readings are in Pa
     altitudeBuffer[loopNum] = bmp.readAltitude(1013.25);// TODO: this should be adjusted
   }
-  
+  //Serial.print(loopNum);
   loopNum = (loopNum + 1) % COLLECTION_POINTS;
   if (loopNum == 0) {
     buffersReady = true;
   }
 
+  
+
   delay(LOOP_DELAY);
 }
 
-float getAverage(float[] list) {
+float getAverage(float list[]) {
   float sum = 0;
   int len = sizeof(list) / sizeof(float);
   for (int i = 0; i < len; i++) {
